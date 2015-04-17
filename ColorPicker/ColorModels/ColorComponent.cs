af4188d3f6c4805ff8f1e26d8e5ce90e8a34211b
AziFarjad <azi.farjad@yahoo.com>
2015-04-17 15:23:03 +1000

    Update ColorComponent.cs

--- ColorPicker/ColorModels/ColorComponent.cs
+++ ColorPicker/ColorModels/ColorComponent.cs
@@ -15,7 +15,7 @@ namespace ColorPicker.ColorModels
         public abstract int MinValue { get; }
 
        //The value of the component for a given color
-        public abstract int Value(Color color);
+        public abstract double Value(Color color);
 
        //The name of the color component (used to avoid reflection)
         public abstract string Name{ get; }
